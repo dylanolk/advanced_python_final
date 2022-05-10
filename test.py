cca10d824c6a5e96e9314c3d71162b8a8744b59a
dylanolk <dylanolk@users.noreply.github.com>
2022-05-10 18:16:41 -0400

    added settings menu class and restructured code

--- test.py
+++ test.py
@@ -1,36 +1,78 @@
 import sys
 from random import randrange
 
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel, QPushButton
-from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
+from PyQt5.QtWidgets import QLabel, QPushButton, QComboBox
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QGridLayout
 from PyQt5.QtWidgets import QWidget
 from PyQt5.QtCore import Qt
 
 
+class SettingsDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Settings")
+        layout = QGridLayout()
 
-app = QApplication(sys.argv)
-window = QWidget()
-window.setWindowTitle('Dice Game')
-window.resize(200,100)
-
-
-layout= QVBoxLayout()
-
-
-def refresh_die():
-    result.setText(str(randrange(6)+1))
-     
-result = QLabel(window)
-layout.addWidget(result, alignment= Qt.AlignHCenter)
-
-button1= QPushButton(window)
-button1.setText("Roll Die")
-button1.clicked.connect(refresh_die)
-layout.addWidget(button1)
+        layout.addWidget(QComboBox(self))
+        
 
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        #initializng window
+        super().__init__()
+        self.setWindowTitle('Dice Game')
+        self.resize(200,100)
 
-window.setLayout(layout)
-window.show()
-sys.exit(app.exec_())
+        #ininitializing middle layout
+        middle_widget = QWidget()
+        middle_layout= QVBoxLayout()
+
+        #result
+        self.result = QLabel(middle_widget)
+        middle_layout.addWidget(self.result, alignment= Qt.AlignHCenter)
+
+        #button
+        button1= QPushButton(middle_widget)
+        button1.setText("Roll Die")
+        button1.clicked.connect(self.refresh_die)
+        middle_layout.addWidget(button1)
+        middle_widget.setLayout(middle_layout)
+
+        #initialize top bar
+        top_widget= QWidget()
+        top_layout= QHBoxLayout()
+
+        #settings button
+        settings_button= QPushButton(top_widget)
+        settings_button.setText("Settings")
+        settings_button.clicked.connect(self.settings_switch)
+
+        top_layout.addWidget(settings_button, alignment=Qt.AlignRight)
+        top_widget.setLayout(top_layout)
+
+        #initialize final layout
+        central_widget= QWidget()
+        final_layout= QVBoxLayout()
+        final_layout.addWidget(top_widget)
+        final_layout.addWidget(middle_widget)
+        central_widget.setLayout(final_layout)
+        self.setCentralWidget(central_widget)
+
+    def refresh_die(self):
+        self.result.setText(str(randrange(6)+1))
+    
+    def settings_switch(self):
+        settings = SettingsDialog()
+        settings.exec()
+
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
